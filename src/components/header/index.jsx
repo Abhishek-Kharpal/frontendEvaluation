eea755bdfca316a88637acd01a202b13227a305c
Abhishek <abhikharpal@gmail.com>
2023-03-01 16:44:29 +0530

    feat: add navigation in header

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,9 +1,17 @@
+import { useNavigate } from 'react-router-dom';
+import { HOME_ROUTE } from '../../constants/routes';
 import './style.css';
 
 const Header = ()=>{
+  const navigate = useNavigate();
+  
+  const handleNavigate = ()=>{
+    navigate(HOME_ROUTE);
+  }
+
   return (
     <div className='header basic-padding' style={{backgroundColor: '#800080'}}>
-      <p><strong>EVENTIFY</strong></p>
+      <p><strong onClick={handleNavigate}>EVENTIFY</strong></p>
     </div>
   );
 };
