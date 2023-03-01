83e1a820adddd89ab2edd796190f53c3de3d3147
Abhishek <abhikharpal@gmail.com>
2023-03-01 13:07:36 +0530

    feat: add responsiveness to search

--- src/components/content/search/index.jsx
+++ src/components/content/search/index.jsx
@@ -3,7 +3,7 @@ import './style.css';
 const Search = ()=>{
   return (
     <div className='search'>
-      <input type="text" placeholder='EVENT NAME' style={{border: '1px solid #800080',marginLeft:'10px', height: '28px', width: '100px', borderRadius: '5px'}}/>
+      <input type="text" placeholder='EVENT NAME' style={{border: '1px solid #800080',marginLeft:'10px', height: '28px', borderRadius: '5px'}}/>
       <i className='fa fa-search' style={{fontSize:'24px',color: '#800080', marginLeft: '5px' }}></i>
     </div>
   );
