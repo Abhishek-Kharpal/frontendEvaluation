cf772288a01fb5ee6d87b9293543d1a05cfe8210
Abhishek <abhikharpal@gmail.com>
2023-03-01 15:58:41 +0530

    chore: make logo responsive

--- src/components/content/search/index.jsx
+++ src/components/content/search/index.jsx
@@ -4,7 +4,7 @@ const Search = ()=>{
   return (
     <div className='search basic-padding'>
       <input type="text" placeholder='EVENT NAME' style={{border: '1px solid #800080',marginLeft:'10px', height: '28px', borderRadius: '5px'}}/>
-      <i className='fa fa-search' style={{fontSize:'24px',color: '#800080', marginLeft: '5px' }}></i>
+      <i className='fa fa-search' style={{fontSize:'24px',color: '#800080', marginLeft: '2px' }}></i>
     </div>
   );
 };
