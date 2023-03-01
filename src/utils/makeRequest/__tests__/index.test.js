618651e81a867bedde5b64908b2de00f532f857e
Abhishek <abhikharpal@gmail.com>
2023-03-01 17:56:07 +0530

    chore: basic test scenario for makeRequest

---
+++ src/utils/makeRequest/__tests__/index.test.js
@@ -0,0 +1,10 @@
+jest.mock('axios');
+
+describe('makeRequest', () => {
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  xit('should make API call with appropriate request options and return response body when only endpoint is specified', async () => {
+  });
+});
