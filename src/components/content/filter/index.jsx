e4fa55deaf1f3f67a9e14e35b0c3869447d3fbca
Abhishek <abhikharpal@gmail.com>
2023-03-01 13:01:00 +0530

    feat: add search icon

--- src/components/content/filter/index.jsx
+++ src/components/content/filter/index.jsx
@@ -3,9 +3,9 @@ import './style.css';
 const Filter = ()=>{
   return (
     <div className='filter'>
-      <i className='fa fa-filter' style={{fontSize:'34px', color: '#800080'}}></i>
+      <i className='fa fa-filter' style={{fontSize:'28px', color: '#800080'}}></i>
       <p>FILTER</p>
-      <i className='fa fa-chevron-down' style={{fontSize: '28px', marginLeft: '5px',color: '#800080', cursor: 'pointer'}}></i>
+      <i className='fa fa-chevron-down' style={{fontSize: '24px', marginLeft: '5px',color: '#800080', cursor: 'pointer'}}></i>
     </div>
   );
 };
--- src/components/content/search/index.jsx
+++ src/components/content/search/index.jsx
@@ -2,8 +2,9 @@ import './style.css';
 
 const Search = ()=>{
   return (
-    <div>
-
+    <div className='search'>
+      <input type="text" placeholder='EVENT NAME' style={{border: '1px solid #800080',marginLeft:'10px', height: '28px', width: '100px', borderRadius: '5px'}}/>
+      <i className='fa fa-search' style={{fontSize:'24px',color: '#800080', marginLeft: '5px' }}></i>
     </div>
   );
 };
